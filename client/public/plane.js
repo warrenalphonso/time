238f91afdd24c684c2408817449a18f15ba75c16
Anthony Russo <arusso3@csub.edu>
2020-03-01 17:19:59 -0800

    Updated plane.js to include a flat plane and began work on lighting.

--- client/public/plane.js
+++ client/public/plane.js
@@ -2,30 +2,57 @@ var scene = new THREE.Scene()
 var camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 )
 
 var renderer = new THREE.WebGLRenderer()
+renderer.shadowMap.enabled = true
+renderer.shadowMap.type = THREE.PCFSoftShadowMap
+// default THREE.PCFShadowMap
+
 renderer.setSize( window.innerWidth, window.innerHeight )
 document.body.appendChild( renderer.domElement )
 
 var geometry = new THREE.BoxGeometry()
-var material = new THREE.MeshBasicMaterial( { color: 0xff0000 } )
+var material = new THREE.MeshBasicMaterial( { color: 0xaaaaff } )
 var cube = new THREE.Mesh( geometry, material )
+cube.castShadow = true
+cube.receiveShadow = false // default
 scene.add( cube )
 
-var planeGeo = new THREE.PlaneGeometry( 5, 5)
-var planeMat = new THREE.MeshBasicMaterial( {color: 0xffff00, side: THREE.DoubleSide} )
+var planeGeo = new THREE.PlaneGeometry( 2000, 2000)
+var planeMat = new THREE.MeshBasicMaterial( {color: 0x228b22, side: THREE.DoubleSide} )
 var plane = new THREE.Mesh( planeGeo, planeMat );
- plane.rotateX(22 / 14)
+ plane.rotateX( 22 / 14 )
+plane.receiveShadow = true
 scene.add( plane )
 
-camera.position.z = 5
+camera.position.z = 10
 camera.position.x = 0
+camera.position.y = 5
+camera.lookAt( new THREE.Vector3( 0, 0, 0 ))
 
-camera.lookAt( new THREE.Vector3(0, 0, 0))
+// ThreeJS Aces helper for test / debug
+var axesHelper = new THREE.AxesHelper( 5 )
+// x is RED, y is GREEN, Z is BLUE
+
+scene.add( axesHelper )
+
+cube.position.y = 2
+
+
+var sun = new THREE.PointLight( 0xffffdd, 1, 0)
+sun.position.set( 0, 10, 0 )
+sun.castShadow = true
+scene.add( sun )
+
+// Set up shadow properties for the light
+sun.shadow.mapSize.width = 512
+sun.shadow.mapSize.height = 512
+sun.shadow.camera.near = 0.5
+sun.shadow.camera.far = 500
 
 function animate() {
 	requestAnimationFrame( animate )
-
 	cube.rotation.x += 0.01
 	cube.rotation.y -= 0.05
+
 	renderer.render( scene, camera )
 }
 
