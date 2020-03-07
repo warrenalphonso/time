98e5861e621d582d00ee6c2afea4f4461e536535
Anthony Russo <arusso3@csub.edu>
2020-03-07 01:02:20 -0800

    Added checkerboard tiles to the plane for depth. Added red cube to begin working on movement inputs.

--- client/public/plane.js
+++ client/public/plane.js
@@ -9,23 +9,30 @@ renderer.shadowMap.type = THREE.PCFSoftShadowMap
 renderer.setSize( window.innerWidth, window.innerHeight )
 document.body.appendChild( renderer.domElement )
 
-var geometry = new THREE.BoxGeometry()
+scene.fog = new THREE.Fog( 0xcce0ff, 500, 10000 )
+
+var geometry = new THREE.BoxGeometry( 30, 30, 30)
 var material = new THREE.MeshBasicMaterial( { color: 0xaaaaff } )
 var cube = new THREE.Mesh( geometry, material )
 cube.castShadow = true
 cube.receiveShadow = false // default
 scene.add( cube )
 
-var planeGeo = new THREE.PlaneGeometry( 2000, 2000)
+var checkeredFloor = new THREE.TextureLoader().load( '/public/checkerboard.jpg' )
+checkeredFloor.wrapS = checkeredFloor.wrapT = THREE.RepeatWrapping
+checkeredFloor.repeat.set( 10, 10 )
+var floorMaterial = new THREE.MeshBasicMaterial( { map: checkeredFloor, side: THREE.DoubleSide } )
+
+var planeGeo = new THREE.PlaneGeometry( 2000, 2000, 10, 10 )
 var planeMat = new THREE.MeshBasicMaterial( {color: 0x228b22, side: THREE.DoubleSide} )
-var plane = new THREE.Mesh( planeGeo, planeMat );
+var plane = new THREE.Mesh( planeGeo, floorMaterial );
  plane.rotateX( 22 / 14 )
 plane.receiveShadow = true
 scene.add( plane )
 
-camera.position.z = 10
-camera.position.x = 0
-camera.position.y = 5
+camera.position.z = 50
+camera.position.x = 50
+camera.position.y = 60
 camera.lookAt( new THREE.Vector3( 0, 0, 0 ))
 
 // ThreeJS Aces helper for test / debug
@@ -34,7 +41,7 @@ var axesHelper = new THREE.AxesHelper( 5 )
 
 scene.add( axesHelper )
 
-cube.position.y = 2
+cube.position.y = 20
 
 
 var sun = new THREE.PointLight( 0xffffdd, 1, 0)
@@ -47,6 +54,13 @@ sun.shadow.mapSize.width = 512
 sun.shadow.mapSize.height = 512
 sun.shadow.camera.near = 0.5
 sun.shadow.camera.far = 500
+
+// Clone a cube to test motion inputs
+var redMat = new THREE.MeshBasicMaterial( { color: 0xff0000 } )
+var moveCube = new THREE.Mesh( geometry, redMat )
+scene.add( moveCube )
+moveCube.position.y = 15
+
 
 function animate() {
 	requestAnimationFrame( animate )
